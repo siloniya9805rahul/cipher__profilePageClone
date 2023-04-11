817d2a06758c714294eb9f9141003ea573314021
Rahul <rahulsiloniya9805@gmail.com>
2023-04-11 15:11:33 +0530

    sec commit

--- cipher_profile_clone/src/App.js
+++ cipher_profile_clone/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      Cipher profile page clone
+      <Navbar />
     </div>
   );
 }
